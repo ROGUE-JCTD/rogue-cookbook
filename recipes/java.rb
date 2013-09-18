96252f855f8c3106f9002ba3656b224a5df244fb
Tyler Garner <garnertb@gmail.com>
2013-09-18 13:33:27 -0400

    Fixes issue where java recipe wouldn't install the oracle Java.

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,4 +1,11 @@
 node.set['java']['oracle']['accept_oracle_download_terms'] = true
 node.set['java']['install_flavor']= "oracle"
 
+include_recipe "java::default"
 include_recipe "java::oracle"
+
+jdk = File.join(node['java']['java_home'], 'jdk1.6.0_45')
+
+execute "Setting JAVA_HOME environmental variable." do
+  command 'echo JAVA_HOME="'+ jdk +'" >> /etc/profile && echo "PATH=$PATH:$JAVA_HOME/bin" >> /etc/profile'
+end
