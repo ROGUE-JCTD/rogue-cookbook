b1d4862631ea9fe287b938eb7e6d90e689a7db24
Tyler Garner <garnertb@gmail.com>
2013-10-16 12:42:57 -0400

    Adds the .geogitconfig to tomcat's home dir.

--- recipes/geogit.rb
+++ recipes/geogit.rb
@@ -8,13 +8,23 @@ package "maven" do
     action :install
 end
 
-execute "install GeoGIT" do
-  command "mvn clean install -DskipTests -X"
-  cwd ::File.join(node['rogue']['geogit']['location'], 'src/parent')
-  user 'root'
-end
+#execute "install GeoGIT" do
+#  command "mvn clean install -DskipTests -X"
+#  cwd ::File.join(node['rogue']['geogit']['location'], 'src/parent')
+#  user 'root'
+#end
 
 file "/etc/profile.d/geogit.sh" do
   content "export GEOGIT_HOME=#{::File.join(node['rogue']['geogit']['location'], 'src/cli-app/target/geogit')} && PATH=$PATH:$GEOGIT_HOME/bin"
   mode 00755
+end
+
+file ::File.join(node["tomcat"]["home"], '.geogitconfig') do
+content <<-EOH
+[user]
+name = Rogue
+email = rogue@lmnsolutions.com
+  EOH
+ mode 00755
+ only_if do node["tomcat"]["home"] end
 end
