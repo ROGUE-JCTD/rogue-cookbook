dd09608e210e61f430ce325d282094ea1277ab50
Tyler Garner <garnertb@gmail.com>
2013-10-16 13:12:45 -0400

    Re-enabled the GeoGit installation.

--- recipes/geogit.rb
+++ recipes/geogit.rb
@@ -8,11 +8,11 @@ package "maven" do
     action :install
 end
 
-#execute "install GeoGIT" do
-#  command "mvn clean install -DskipTests -X"
-#  cwd ::File.join(node['rogue']['geogit']['location'], 'src/parent')
-#  user 'root'
-#end
+execute "install GeoGIT" do
+  command "mvn clean install -DskipTests"
+  cwd ::File.join(node['rogue']['geogit']['location'], 'src/parent')
+  user 'root'
+end
 
 file "/etc/profile.d/geogit.sh" do
   content "export GEOGIT_HOME=#{::File.join(node['rogue']['geogit']['location'], 'src/cli-app/target/geogit')} && PATH=$PATH:$GEOGIT_HOME/bin"
