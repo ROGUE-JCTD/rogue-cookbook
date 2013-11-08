1fc7ab8bcdaa9d5ac4d2f459c30484293273f26c
Tyler Garner <garnertb@gmail.com>
2013-11-08 12:11:15 -0500

    Bumped the cookbook version.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'rogue-cookbook'
 description      'Installs/Configures the ROGUE JCTD project'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends "nginx"
 depends "java"
