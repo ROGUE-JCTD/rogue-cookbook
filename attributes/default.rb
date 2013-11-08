b9fadad7fc424b947879ee1c4a5873031e01ab55
Tyler Garner <garnertb@gmail.com>
2013-11-08 10:44:00 -0500

    Changes the tomat template to use a fqdn vs ip.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,15 @@
-
-default['rogue']['web_server'] = 'nginx'
 default['rogue']['debug'] = true
+default['rogue']['iface'] = 'eth0'
+
+if node.attribute?('vagrant')
+    default['rogue']['iface'] = 'eth1'
+end
+
+default['rogue']['ip'] = node[:network][:interfaces][node['rogue']['iface']][:addresses].detect{|k,v| v[:family] == "inet" }[0]
 
 default['rogue']['networking']['application']['hostname'] = 'rogue-geoserver'
-default['rogue']['networking']['application']['address'] = node[:network][:interfaces][:eth1][:addresses].detect{|k,v| v[:family] == "inet" }[0]
+default['rogue']['networking']['application']['address'] = node['rogue']['ip']
+default['rogue']['networking']['application']['fqdn'] = node['rogue']['ip']
 default['rogue']['networking']['application']['gateway'] = nil
 default['rogue']['networking']['application']['netmask'] = nil
 
@@ -11,6 +17,13 @@ default['rogue']['networking']['database']['hostname'] = 'rogue-database'
 default['rogue']['networking']['database']['address'] = '127.0.0.1'
 default['rogue']['networking']['database']['gateway'] = nil
 default['rogue']['networking']['database']['netmask'] = nil
+
+default['rogue']['geoserver']['base_url'] = '/geoserver'
+default['rogue']['geoserver']['data_dir'] = '/var/lib/geoserver_data'
+default['rogue']['geoserver']['geowebcache']['url'] = "http://sourceforge.net/projects/geowebcache/files/geowebcache/1.4.0/geowebcache-1.4.0-war.zip"
+default['rogue']['geoserver']['jai']['url'] = "http://download.java.net/media/jai/builds/release/1_1_3/jai-1_1_3-lib-linux-amd64-jdk.bin"
+default['rogue']['geoserver']['jai_io']['url'] = "http://download.java.net/media/jai-imageio/builds/release/1.1/jai_imageio-1_1-lib-linux-amd64-jdk.bin"
+default['rogue']['geoserver']['url']= "http://#{node['rogue']['networking']['application']['fqdn']}#{node['rogue']['geoserver']['base_url']}/"
 
 
 default['rogue']['settings']['ALLOWED_HOSTS'] = [node['rogue']['networking']['application']['address'], 'localhost']
@@ -24,8 +37,8 @@ default['rogue']['rogue_geonode']['branch'] = 'master'
 default['rogue']['rogue_geonode']['location'] = File.join(node['rogue']['geonode']['location'], 'rogue_geonode')
 default['rogue']['rogue_geonode']['url'] = 'https://github.com/ROGUE-JCTD/rogue_geonode.git'
 
-default['rogue']['rogue_geonode']['settings']['SITEURL'] = "http://#{node['rogue']['networking']['application']['address']}:8000/"
-default['rogue']['rogue_geonode']['settings']['OGC_SERVER']['LOCATION'] = "http://#{node['rogue']['networking']['application']['address']}/geoserver/"
+default['rogue']['rogue_geonode']['settings']['SITEURL'] = "http://#{node['rogue']['networking']['application']['fqdn']}/"
+default['rogue']['rogue_geonode']['settings']['OGC_SERVER']['LOCATION'] = node['rogue']['geoserver']['url']
 default['rogue']['rogue_geonode']['settings']['OGC_SERVER']['DATASTORE'] = ""
 default['rogue']['rogue_geonode']['settings']['OGC_SERVER']['GEOGIT_DATASTORE_DIR'] = "/data/geogit/"
 default['rogue']['rogue_geonode']['settings']['OGC_SERVER']['USER'] = "admin"
@@ -45,8 +58,3 @@ default[:postgis][:version] = '2.0.4'
 default['postgis']['template_name'] = 'template_postgis'
 default['postgis']['locale'] = 'en_US.utf8'
 
-default['rogue']['geoserver']['base_url'] = '/geoserver'
-default['rogue']['geoserver']['data_dir'] = '/var/lib/geoserver_data'
-default['rogue']['geoserver']['geowebcache']['url'] = "http://sourceforge.net/projects/geowebcache/files/geowebcache/1.4.0/geowebcache-1.4.0-war.zip"
-default['rogue']['geoserver']['jai']['url'] = "http://download.java.net/media/jai/builds/release/1_1_3/jai-1_1_3-lib-linux-amd64-jdk.bin"
-default['rogue']['geoserver']['jai_io']['url'] = "http://download.java.net/media/jai-imageio/builds/release/1.1/jai_imageio-1_1-lib-linux-amd64-jdk.bin"
