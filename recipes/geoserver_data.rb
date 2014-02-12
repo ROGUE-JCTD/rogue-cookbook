845b9442b3365d865a8bd5c5d0b0f2d9c220cca1
Tyler Garner <garnertb@gmail.com>
2014-02-12 12:42:05 -0500

    Create the file-service-store if it doesn't exist.

--- recipes/geoserver_data.rb
+++ recipes/geoserver_data.rb
@@ -7,25 +7,32 @@ git geoserver_data_dir do
 end
 
 dirs = "#{node['rogue']['geoserver']['data_dir']} #{File.join(node['rogue']['geoserver']['data_dir'], 'geogit')} #{File.join(node['rogue']['geoserver']['data_dir'], 'file-service-store')}"
+geogit = File.join(node['rogue']['geoserver']['data_dir'], 'geogit')
+file_store = File.join(node['rogue']['geoserver']['data_dir'],'file-service-store')
 
 # move the geoserver data dir to the correct location
 execute "copy_geoserver_data_dir" do
   command <<-EOH
-    mv #{geoserver_data_dir} #{node['rogue']['geoserver']['data_dir']}
-    chown -R #{node['tomcat']['user']}:roguecat #{dirs}
-    chmod -R 775 #{node['rogue']['geoserver']['data_dir']} #{dirs}
+    cp -R #{geoserver_data_dir} #{node['rogue']['geoserver']['data_dir']}
   EOH
   action :run
   only_if  do !geoserver_data_dir.eql? node['rogue']['geoserver']['data_dir'] and File.exists? geoserver_data_dir and !File.exists? node['rogue']['geoserver']['data_dir'] end
+  notifies :create, "directory[#{file_store}]", :immediately
   notifies :run, "execute[change_perms]"
   user 'root'
 end
 
-geogit = File.join(node['rogue']['geoserver']['data_dir'], 'geogit')
-file_store = File.join(node['rogue']['geoserver']['data_dir'],'file-service-store')
+directory file_store do
+  owner node['tomcat']['user']
+  group 'roguecat'
+  action :create
+  not_if { File.exists? file_store }
+end
 
 execute "change_perms" do
   command <<-EOH
+    chown -R #{node['tomcat']['user']}:roguecat #{dirs}
+    chmod -R 775 #{node['rogue']['geoserver']['data_dir']} #{dirs}
     chmod g+s #{geogit} #{file_store}
     setfacl -d -m g::rwx #{geogit}
     setfacl -d -m o::rx #{geogit}
