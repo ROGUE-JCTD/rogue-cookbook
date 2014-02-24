aa28f66807253c5761b5450359a30f287f14e289
Tyler Garner <garnertb@gmail.com>
2014-02-24 17:44:50 -0500

    Rename the username variable to user_name in the geonode_authorize_layer function.

--- recipes/database.rb
+++ recipes/database.rb
@@ -52,7 +52,8 @@ postgresql_database 'add_geonode_authorize_layer_function' do
   connection   postgresql_connection_info
   database_name geonode_connection_info[:name]
   sql <<-EOH
-CREATE OR REPLACE FUNCTION geonode_authorize_layer(username varchar, type_name varchar) RETURNS varchar AS $$
+DROP FUNCTION geonode_authorize_layer(varchar, varchar);
+CREATE OR REPLACE FUNCTION geonode_authorize_layer(user_name varchar, type_name varchar) RETURNS varchar AS $$
 
 DECLARE
 view_perm integer;
@@ -69,8 +70,8 @@ if (not FOUND) then
 	-- no layer
 	return 'nl';
 end if;
-if (username IS NOT NULL) then
-	SELECT INTO user * FROM "auth_user" WHERE "auth_user"."username" = username;
+if (user_name IS NOT NULL) then
+SELECT INTO user * FROM "auth_user" WHERE "auth_user"."username" = user_name;
 	if (not FOUND) then
 		-- no user
 		return 'nu';
@@ -130,7 +131,7 @@ PERFORM "security_genericobjectrolemapping"."object_id"
 	);
 if (FOUND) then return 'gr-ro'; end if;
 
-if (username IS NOT NULL) then
+if (user_name IS NOT NULL) then
 	-- user role, read-write
 	PERFORM "security_userobjectrolemapping"."object_id"
 		FROM "security_userobjectrolemapping"
