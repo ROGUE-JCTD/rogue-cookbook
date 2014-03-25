da7c931a93000a0fdf09fc7ee8dd20807104677b
Tyler Garner <garnertb@gmail.com>
2014-03-25 11:09:48 -0400

    Add dependencies to metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,15 @@ description      'Installs/Configures the ROGUE JCTD project'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.4'
 
-depends "nginx"
-depends "java"
-depends "tomcat"
-depends "hostsfile"
-depends "postgresql"
-depends "database"
-depends "ohai"
+depends 'apt', '~> 2.1.1'
+depends 'build-essential', '~> 1.4.2'
+depends 'git', '~> 2.6.0'
+depends 'python', '~> 1.4.2'
+depends 'openssl', '~> 1.1.0'
+depends 'ohai', '~> 1.1.12'
+depends 'hostsfile', '~> 2.4.1'
+depends 'database', '~> 1.5.2'
+depends 'java','~> 1.22.0'
+depends 'tomcat', '~> 0.15.2'
+depends 'postgresql', '~> 3.3.4'
+depends 'nginx', '~> 2.0.4'
