01e7f161aa611eca0df3f91250da5248e630a029
Tyler Garner <garnertb@gmail.com>
2014-06-17 13:52:41 -0400

    Default deployment to Java 7.  Fixes #22.

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,7 +1,6 @@
 node.default['java']['oracle']['accept_oracle_download_terms'] = true
 node.default['java']['install_flavor']= "oracle"
-node.default['java']['jdk_version'] = '6'
-node.default['java']['java_home']='/usr/lib/jvm/jdk1.6.0_45'
+node.default['java']['jdk_version'] = '7'
 
 include_recipe "java::default"
 include_recipe "java::oracle"
--- test/integration/default/serverspec/java_spec.rb
+++ test/integration/default/serverspec/java_spec.rb
@@ -4,5 +4,5 @@ include Serverspec::Helper::Exec
 include Serverspec::Helper::DetectOS
 
 describe command('java -version') do
-  its(:stdout){ should include 'java version "1.6.0_45"' }
+  its(:stdout){ should include 'java version "1.7' }
 end
