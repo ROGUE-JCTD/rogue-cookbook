4c8fa9074b1ed53e5e11b53a29aa3bbcef19828d
Tyler Garner <garnertb@gmail.com>
2014-09-24 14:34:42 -0400

    Add an IAFF recipe and IAFF attributes.

---
+++ attributes/iaff.rb
@@ -0,0 +1,3 @@
+default['rogue']['iaff_geoshape']['branch'] = 'master'
+default['rogue']['iaff_geoshape']['location'] = File.join(node['rogue']['geonode']['location'], 'iaff_geoshape')
+default['rogue']['iaff_geoshape']['url'] = 'https://github.com/ROGUE-JCTD/iaff_geoshape.git'
