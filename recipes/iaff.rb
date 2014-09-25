b1ade27e74630c1493e038904b6e0b8d3310b407
Tyler Garner <garnertb@gmail.com>
2014-09-25 09:17:00 -0400

    Use pip to install iaff_geoshape vs installing from source and update the update_data cronjob to run every 30 minutes.

--- recipes/iaff.rb
+++ recipes/iaff.rb
@@ -1,21 +1,10 @@
-git node['rogue']['iaff_geoshape']['location'] do
-  repository node['rogue']['iaff_geoshape']['url']
-  revision node['rogue']['iaff_geoshape']['branch']
-  action :sync
-end
-
-python_pip node['rogue']['iaff_geoshape']['location'] do
-  virtualenv node['rogue']['geonode']['location']
-  options "-e"
-end
-
 execute "sync_db" do
   command "#{node['rogue']['geonode']['location']}bin/python #{node['rogue']['rogue_geonode']['location']}/manage.py syncdb --database=geonode_imports --no-initial-data --all"
 end
 
 
 file "/etc/cron.d/geoshape_update_data" do
-  content "* 30 * * * rogue /var/lib/geonode/bin/python /var/lib/geonode/rogue_geonode/manage.py update_data\n"
+  content "*/30 * * * * rogue /var/lib/geonode/bin/python /var/lib/geonode/rogue_geonode/manage.py update_data > /dev/null\n"
   mode 00755
   action :create_if_missing
 end
