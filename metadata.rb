57a047854490dad7e2f3fdc4f21718dee2682272
Tyler Garner <garnertb@gmail.com>
2015-01-21 08:00:49 -0500

    Update the version number to match the next release.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'rogue'
 description      'Installs/Configures the ROGUE JCTD project'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '1.2'
 
 depends 'nginx', '~> 2.7.4'
 depends 'apt', '~> 2.2'
